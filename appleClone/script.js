0cbb951c57c97eafbbb1a3ec7df7db9fe23bc8e6
Swarnava <swarnavamudi22@gmail.com>
2023-09-10 23:46:15 +0530

    Page 10-11-12 is done

--- appleClone/script.js
+++ appleClone/script.js
@@ -395,3 +395,115 @@ function canvas() {
     });
 }
 canvas();
+
+function canvas1() {
+    const canvas = document.querySelector("#page12>canvas");
+    const context = canvas.getContext("2d");
+
+    canvas.width = window.innerWidth;
+    canvas.height = window.innerHeight;
+
+
+    window.addEventListener("resize", function () {
+        canvas.width = window.innerWidth;
+        canvas.height = window.innerHeight;
+        render();
+    });
+
+    function files(index) {
+        var data = `
+  .//images/Vision00001.png
+  .//images/Vision00002.png
+  .//images/Vision00003.png
+  .//images/Vision00004.png
+  .//images/Vision00005.png
+  .//images/Vision00006.png
+  .//images/Vision00007.png
+  .//images/Vision00008.png
+  .//images/Vision00009.png
+  .//images/Vision00010.png
+  .//images/Vision00011.png
+  .//images/Vision00012.png
+  .//images/Vision00013.png
+  .//images/Vision00014.png
+  .//images/Vision00015.png
+  .//images/Vision00016.png
+  .//images/Vision00017.png
+  .//images/Vision00018.png
+  .//images/Vision00019.png
+  .//images/Vision00020.png
+  .//images/Vision00021.png
+  .//images/Vision00022.png
+  .//images/Vision00023.png
+  .//images/Vision00024.png
+  .//images/Vision00025.png
+  `;
+        return data.split("\n")[index];
+    }
+
+    const frameCount = 25;
+
+    const images = [];
+    const imageSeq = {
+        frame: 1,
+    };
+
+    for (let i = 0; i < frameCount; i++) {
+        const img = new Image();
+        img.src = files(i);
+        images.push(img);
+    }
+
+    gsap.to(imageSeq, {
+        frame: frameCount - 1,
+        snap: "frame",
+        ease: `none`,
+        scrollTrigger: {
+            scrub: 0.15,
+            trigger: `#page12`,
+            //   set start end according to preference
+            start: `top top`,
+            end: `80% top`,
+            scroller: `#main`,
+        },
+        onUpdate: render,
+    });
+
+    images[1].onload = render;
+
+    function render() {
+        scaleImage(images[imageSeq.frame], context);
+    }
+
+    function scaleImage(img, ctx) {
+        var canvas = ctx.canvas;
+        var hRatio = canvas.width / img.width;
+        var vRatio = canvas.height / img.height;
+        var ratio = Math.max(hRatio, vRatio);
+        var centerShift_x = (canvas.width - img.width * ratio) / 2;
+        var centerShift_y = (canvas.height - img.height * ratio) / 2;
+        ctx.clearRect(0, 0, canvas.width, canvas.height);
+        ctx.drawImage(
+            img,
+            0,
+            0,
+            img.width,
+            img.height,
+            centerShift_x,
+            centerShift_y,
+            img.width * ratio,
+            img.height * ratio
+        );
+    }
+    ScrollTrigger.create({
+
+        trigger: "#page12",
+        pin: true,
+        // markers:true,
+        scroller: `#main`,
+        //   set start end according to preference
+        start: `top top`,
+        end: `80% top`,
+    });
+}
+canvas1();
