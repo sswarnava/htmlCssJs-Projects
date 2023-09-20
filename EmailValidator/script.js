d474039ef3e69ac5ec21b4e9df52620e467eee30
Swarnava <swarnavamudi22@gmail.com>
2023-09-20 23:01:07 +0530

    css part is done, Project is almost completed

--- EmailValidator/script.js
+++ EmailValidator/script.js
@@ -1,19 +1,25 @@
 submitBtn.addEventListener("click", async (e) => {
     e.preventDefault()
-    console.log("Clicked!")
-    resultCont.innerHTML = `<img width="123" src="img/loading.svg" alt="">`
-    let key = "EnterYourKey"
+    // console.log("Clicked!")
+    resultCont.innerHTML = `<img src="./loading.gif" alt="" srcset="">`
+    let key = "Enter Your Key"
     let email = document.getElementById("userEmail").value
     let url = `https://api.emailvalidation.io/v1/info?apikey=${key}&email=${email}`
     let res = await fetch(url)
     let result = await res.json()
     let str = ``
-    for (key of Object.keys(result)) {
-        if (result[key] !== "" && result[key] !== " ") {
-            str = str + `<div>${key}: ${result[key]}</div>`
-        }
+    if (result.state === 'deliverable' && result.smtp_check === true) {
+        str = `<div class="result">Status- OK <br> It is Valid Email</div>`
+    }
+    else {
+        str = `<div class="noResult">Status- Not Found <br> It is Not a Valid Email</div>`
     }
 
-    console.log(str)
+    // console.log(str)
+    userEmail.value = ""
     resultCont.innerHTML = str
 })
+
+// state:
+// smtp_check:
+// deliverable
