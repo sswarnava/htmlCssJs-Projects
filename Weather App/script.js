305e47435a9e23bd12a3d816dcc5b806d64b4968
Swarnava <swarnavamudi22@gmail.com>
2023-11-08 23:29:41 +0530

    api key is remove now

--- Weather App/script.js
+++ Weather App/script.js
@@ -1,9 +1,6 @@
-// const url = 'https://weather-by-api-ninjas.p.rapidapi.com/v1/weather?city=Seattle';
-
 const options = {
     method: 'GET',
     headers: {
-        'X-RapidAPI-Key': 'b4634041f8mshedba667d622017ap1bd867jsne7424e07211d',
         'X-RapidAPI-Host': 'weather-by-api-ninjas.p.rapidapi.com'
     }
 };
